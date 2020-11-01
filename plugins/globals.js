c14f72b9c1c8fe5d77b23dab68b4aa452176b70a
aleksandrvergunov <vergunov.123456@mail.ru>
2020-11-01 21:45:29 +0700

    Modified global plugin

--- plugins/globals.js
+++ plugins/globals.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ru-Ru'
+import lang from 'element-ui/lib/locale/lang/ru-RU'
+import locale from 'element-ui/lib/locale'
 import VueMarkdown from 'vue-markdown'
 
 import DateFilter from '@/common/filters/date.filter'
 
-Vue.use(Element, { locale })
+locale.use(lang)
+Vue.use(Element)
 Vue.component('vue-markdown', VueMarkdown)
 
 Vue.filter('date', DateFilter)
